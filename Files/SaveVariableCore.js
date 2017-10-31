2f083fe31291fddcc7c3b82d6972029e7fc618b8
n2naokun <nn29661000@gmail.com>
2017-10-31 17:24:21 +0900

    大幅な仕様変更

--- Files/SaveVariableCore.js
+++ Files/SaveVariableCore.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2017/10/31 大幅な仕様変更
 // 1.0.0 2017/10/29 初版
 // ----------------------------------------------------------------------------
 // [Twitter]: https://twitter.com/n2naokun/
@@ -16,10 +17,39 @@
  * @plugindesc プラグイン内の変数を簡単にセーブに対応させる前提プラグイン
  * @author n2naokun(柊菜緒)
  *
- * @help セーブしたいオブジェクト = new saveObject(プラグインの名前の文字列);
- * 上記のようにプラグイン内でインスタンスを生成して通常通りに使ってください。
- * プレーヤーがセーブしたタイミングでセーブされます。
+ * @help var インスタンスの名前 = new saveObject("プラグインの名前");
+ * まず上記のようにプラグイン内でインスタンスを生成してください。
+ * セーブしたいデータがある場合は
+ * インスタンスの名前.saveParams以下に新しく変数/オブジェクトを作ってください。
+ * 
+ * 例:（インスタンスの名前がsaveの場合）
+ * save.saveParams.items = 1;
+ * このような感じで使ってください。（オブジェクトも入れれます）
+ * また、使用直前に
+ * var savedata = save.saveParams;
+ * savedata.item = 1;
+ * savedata.item2 = 1;
+ * のように省略することも可能です。
+ * （JavaScriptがオブジェクトを参照渡しすることを利用しています。）
+ * ※使用直前でないと上手く動作しないことがあります。
+ * 
+ * プレーヤーがセーブ/ロードしたタイミングでセーブ/ロードされます。
  * ※セーブごとに内容が変わるので注意。
+ * 
+ * 
+ * 
+ * 
+ * 仕組みの解説
+ * ロードされると自動的にグローバル変数の$saveParams["プラグインの名前"]
+ * が各インスタンスのsaveParamsからアクセスできるように更新されます。
+ * 
+ * 例のようにsave.saveParamsにアクセスすると実際には
+ * $saveParams["プラグインの名前"]にアクセスされます。
+ * var savedata = save.saveParams のように省略する場合は直前に行わないと
+ * 新しい参照先に切り替えが行われず上手く動作しません。
+ * （言語の仕様なのでご容赦ください。）
+ * 
+ * 
  * 
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -35,11 +65,12 @@ Imported.SaveVariableCore = true;
 (function (_global) {
     // セーブ用のオブジェクトを作成
     window.$saveParams = {};
+    window._updates = {};
 
     var DataManager_createGameObjects = DataManager.createGameObjects;
     DataManager.createGameObjects = function () {
         DataManager_createGameObjects.call(this);
-        $saveParams = $saveParams || new saveParams();
+        $saveParams = $saveParams || {};
     }
 
     var DataManager_makeSaveContents = DataManager.makeSaveContents;
@@ -53,24 +84,38 @@ Imported.SaveVariableCore = true;
     DataManager.extractSaveContents = function (contents) {
         DataManager_extractSaveContents.call(this, contents);
         $saveParams = contents.saveParams;
+        for (name in _updates) {
+            // 各インスタンスの参照先を更新
+            _updates[name].update();
+        }
     };
 
-    function saveParams() { }
 })(this);
 
 // セーブオブジェクトの定義
 function saveObject() {
-    this.initialize.apply(this, arguments);
+    return this.initialize.apply(this, arguments);
 }
 
 // セーブオブジェクトの初期化
 saveObject.prototype.initialize = function (name) {
     if (name != null && name != "") {
-        $saveParams[name] = this;
-        // thisのインスタンスへの参照を渡さずに$saveParamsの参照を渡す
-        return $saveParams[name];
+        // _nameにプラグインの名前を格納
+        this._name = name;
+        // 初期化されてなかったら初期化する
+        $saveParams[name] = $saveParams[name] || {};
+        // _paramsに$saveParams[name]をセット
+        this.saveParams = $saveParams[name];
+        // グローバル変数の_updates[name]に自身のインスタンスの参照を格納
+        _updates[name] = this;
     }
     else {
         throw "saveObject name null exception";
     }
-}
+};
+
+saveObject.prototype.update = function () {
+    if (this.saveParams !== $saveParams[this._name]) {
+        this.saveParams = $saveParams[this._name];
+    }
+};
