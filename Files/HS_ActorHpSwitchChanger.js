71e5cfe75cf5608f8e306f1f286abd10451b1dff
n2naokun <nn29661000@gmail.com>
2019-05-11 20:55:10 +0900

    パラメーターを評価するタイミングを追加

--- Files/HS_ActorHpSwitchChanger.js
+++ Files/HS_ActorHpSwitchChanger.js
@@ -7,6 +7,7 @@
 // ----------------------------------------------------------------------------
 // Version
 // 1.0.0 2019/05/10 初版
+// 1.1.0 2019/05/11 パラメーターが評価されるタイミングを追加
 // ----------------------------------------------------------------------------
 // [Twitter]: https://twitter.com/n2naokun/
 // [GitHub] : https://github.com/n2naokun/
@@ -43,6 +44,17 @@
  * <HP==100:22>
  * HPが100ピッタリになるとスイッチ22がONになります。
  * 
+ * アクターに設定したパラメーターが評価されるタイミングは次にあげる範囲です。
+ * ・アクターのHPが増減したタイミング
+ * ・ニューゲームをした直後
+ * ・ゲームをロードした直後
+ * ・バトルに入る直前
+ * ・パーティーメンバーが増えた時
+ * 
+ * 注意：
+ * バトルに参加しないパーティーメンバーのHPも評価されます。
+ * アクターがパーティを抜ける時はアクターに関連するスイッチはすべてOFFになります。
+ * 抜けたアクターが再びパーティに参加するとその時点で再評価されます。
  * 
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -60,14 +72,14 @@ Imported.HS_ActorHpSwitchChanger = true;
 // 他のプラグインとの連携用シンボル
 
 (function (_global) {
-   let Game_Battler_gainHp = Game_Battler.prototype.gainHp;
 
+   let Game_Battler_gainHp = Game_Battler.prototype.gainHp;
    // パラメーターのキャッシュ
    let actorsSwitchParams = [];
    const paramCheck = /^<HP(==|<=|>=|<|>)(\d+)(%|)?:(\d+)>/;
    /*
    param構造
-   [mode(Number), code(String), value(Number), switch(Number)]
+   {mode:Number, code:String, value:Number, switch:Number}
    mode:
       数値として直接処理するか割合として処理するかの値
       0なら数値
@@ -85,50 +97,50 @@ Imported.HS_ActorHpSwitchChanger = true;
    Game_Battler.prototype.gainHp = function (value) {
       Game_Battler_gainHp.apply(this, arguments);
 
-      let hp = this.hp;
-      const mhp = this.mhp;
       const actorId = this.actorId();
-
       // パラメーター解析部
       if (!actorsSwitchParams[actorId]) {
-         paramsInit(actorId);
+         paramInit(actorId);
       }
 
-
       // スイッチ操作部
-      actorsSwitchParams[actorId].forEach(function (param) {
-         const mode = param.mode;
-         const code = param.code;
-         const value = param.value;
-         const switchId = param.switchId;
-         if (mode) { // 1 なら実行される
-            hp = hp / mhp * 100;
-         }
-         let result = false;
-         switch (code) {
-            case "==":
-               result = hp === value;
-               break;
-            case "<=":
-               result = hp <= value;
-               break;
-            case ">=":
-               result = hp >= value;
-               break;
-            case "<":
-               result = hp < value;
-               break;
-            case ">":
-               result = hp > value;
-               break;
-         }
-
-         $gameSwitches.setValue(switchId, result);
-      }.bind(this));
+      actorsSwitchParams[actorId].forEach(switcher.bind(this));
 
    };
 
-   let paramsInit = function (actorId) {
+   let switcher = function (param) {
+      let hp = this.hp;
+      const mhp = this.mhp;
+      const mode = param.mode;
+      const code = param.code;
+      const value = param.value;
+      const switchId = param.switchId;
+      if (mode) { // 1 なら割合モードなので実行される
+         hp = hp / mhp * 100;
+      }
+      let result = false;
+      switch (code) {
+         case "==":
+            result = hp === value;
+            break;
+         case "<=":
+            result = hp <= value;
+            break;
+         case ">=":
+            result = hp >= value;
+            break;
+         case "<":
+            result = hp < value;
+            break;
+         case ">":
+            result = hp > value;
+            break;
+      }
+
+      $gameSwitches.setValue(switchId, result);
+   };
+
+   let paramInit = function (actorId) {
       const note = $dataActors[actorId].note;
       let meta = note.split("\n");
       meta = meta.filter(function (elem) {
@@ -159,15 +171,68 @@ Imported.HS_ActorHpSwitchChanger = true;
    };
 
    let DataManager_onload = DataManager.onLoad;
+   // アクターデータ読み込み完了時にパラメーターを構築
    DataManager.onLoad = function (object) {
-      DataManager_onload.call(this, object);
+      DataManager_onload.apply(this, arguments);
       if (object === $dataActors) {
          $dataActors.forEach(function (actor, actorId, actors) {
             if (actor) {
-               paramsInit(actorId);
+               paramInit(actorId);
             }
          });
       }
+   };
+
+   // ニューゲーム時にパラメーターを評価
+   let DataManager_setupNewGame = DataManager.setupNewGame;
+   DataManager.setupNewGame = function () {
+      DataManager_setupNewGame.call(this);
+      $gameParty.allMembers().forEach(function (actor) {
+         actorsSwitchParams[actor.actorId()].forEach(switcher.bind(actor));
+      });
+   };
+
+   // ロード時にパラメーターを評価
+   let DataManager_loadGame = DataManager.loadGame;
+   DataManager.loadGame = function (savefileId) {
+      const result = DataManager_loadGame.apply(this, arguments);
+      $gameParty.allMembers().forEach(function (actor) {
+         actorsSwitchParams[actor.actorId()].forEach(switcher.bind(actor));
+      });
+      return result;
+   };
+
+   // バトルに入る直前にパラメーターを評価
+   let BattleManager_setup = BattleManager.setup;
+   BattleManager.setup = function (troopId, canEscape, canLose) {
+      $gameParty.allMembers().forEach(function (actor) {
+         actorsSwitchParams[actor.actorId()].forEach(switcher.bind(actor));
+      });
+      BattleManager_setup.apply(this, arguments);
+   };
+
+   // パーティーメンバー追加時にパラメーターを評価
+   let Game_Party_addActor = Game_Party.prototype.addActor;
+   Game_Party.prototype.addActor = function (actorId) {
+      if (!this._actors.contains(actorId)) {
+         Game_Party_addActor.apply(this, arguments);
+         this.allMembers().forEach(function (actor) {
+            actorsSwitchParams[actor.actorId()].forEach(switcher.bind(actor));
+         });
+      }
+   };
+
+   // パーティーメンバー削除時に削除したメンバーに結び付けられたスイッチをOFFに
+   let Game_Party_removeActor = Game_Party.prototype.removeActor;
+   Game_Party.prototype.removeActor = function (actorId) {
+      if (this._actors.contains(actorId)) {
+         const actor = $gameActors.actor(actorId);
+         actorsSwitchParams[actor.actorId()].forEach(function (param) {
+            const switchId = param.switchId;
+            $gameSwitches.setValue(switchId, false);
+         });
+      }
+      Game_Party_removeActor.apply(this, arguments);
    };
 
 })(this);
